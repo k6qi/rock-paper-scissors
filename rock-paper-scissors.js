6e1483eadb8c88400bd8183dada1b5b945b80788
Kevin Qi <kevin@Kevins-MacBook-Pro.local>
2023-10-30 14:42:24 -0400

    feat: add play round function that plays a round of between player and computer

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,24 +1,37 @@
 
 function getComputerChoice(){
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["rock", "paper", "scissors"];
     let move = Math.floor(Math.random() * choices.length );
     return choices[move];
 }
+const computerSelection = getComputerChoice();
+const playerSelection = "rock";
 
 
 function playRound(playerSelection, computerSelection){
     if (
-        (playerSelection === "Rock" && computerSelection === "Scissors") ||
-        (playerSelection === "Paper" && computerSelection === "Rock") ||
-        (playerSelection === "Scissors" && computerSelection === "Paper")
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
     ) {
-        console.log("Player has won!")
+        let resultWon = `You won! ${playerSelection[0].toUpperCase() + playerSelection.slice(1)} beats ${computerSelection[0].toUpperCase() + computerSelection.slice(1)}.`;
+        return resultWon;
     }
     if (
-        (computerSelection === "Rock" && playerSelection === "Scissors") ||
-        (computerSelection === "Paper" && playerSelection === "Rock") ||
-        (computerSelection === "Scissors" && playerSelection === "Paper")
+        (computerSelection === "rock" && playerSelection === "scissors") ||
+        (computerSelection === "paper" && playerSelection === "rock") ||
+        (computerSelection === "scissors" && playerSelection === "paper")
     ){
-        console.log("Computer has won!")
+        let resultLost = `You lost! ${computerSelection[0].toUpperCase() + computerSelection.slice(1) } beats ${playerSelection[0].toUpperCase() + playerSelection.slice(1)}`;
+        return resultLost;
+    }
+    if (playerSelection === computerSelection){
+        let resultTie = "Its a tie!";
+        return resultTie;
+        
     }
 }
+
+
+console.log(playRound(playerSelection, computerSelection));
+
